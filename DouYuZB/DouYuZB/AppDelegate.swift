6127db2f4a849d66e77eea69e7ca666449e83341
camel <camelfu@icloud.com>
2020-12-03 22:44:25 +0800

    init

---
+++ DouYuZB/DouYuZB/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DouYuZB
+//
+//  Created by Camel on 2020/12/3.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
